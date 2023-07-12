8e4a02268a97f05181404573f6a1f851006630aa
Kenith Chan <kenith.ckl@gmail.com>
2023-07-12 16:07:47 -0400

    test a new bot with 2000 words checking

---
+++ discordbot2.js
@@ -0,0 +1,70 @@
+// Initialize dotenv
+require('dotenv').config();
+
+// Discord.js versions ^13.0 require us to explicitly define client intents
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ] });
+
+client.on('ready', () => {
+ console.log(`Logged in as ${client.user.tag}!`);
+});
+
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+client.on("messageCreate", async function (message) {
+    if (message.author.bot) return;
+    const prefix  = "/gpt";
+    if (!message.content.startsWith(prefix)) return;
+
+    prompt = message.content.slice(prefix.length)
+
+    const userQuery = prompt;
+    console.log("prompt: ", userQuery);
+    try {
+        const response = await openai.createChatCompletion({
+            model:"gpt-3.5-turbo",
+            messages: [
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": userQuery}
+            ],
+        });
+        const generatedText = response.data.choices[0].message.content;
+
+        // Check if the generated text is longer than the Discord message limit
+        const discordMessageLimit = 2000;
+        if (generatedText.length > discordMessageLimit) {
+            // If it is, split it into multiple messages and send them one by one
+            let start = 0;
+            while (start < generatedText.length) {
+                let chunk = generatedText.substring(start, Math.min(start + discordMessageLimit, generatedText.length));
+                await message.channel.send(chunk);
+                start += discordMessageLimit;
+            }
+        } else {
+            // If it isn't, just send the message as is
+            return message.channel.send(generatedText);
+        }
+    } catch (error) {
+        if (error.response) {
+          console.log(error.response.status);
+          console.log(error.response.data);
+        } else {
+          console.log(error.message);
+        }
+        return message.reply(
+        "Sorry, something went wrong. I am unable to process your query."
+        );
+    }
+});
+
+// Log In our bot
+client.login(process.env.BOT_TOKEN);
